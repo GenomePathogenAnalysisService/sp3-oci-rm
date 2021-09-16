b012abf19d641a9a029cb8ed095eea14609e17a4
bhaworth <ben@benhaworth.com>
2021-09-16 14:10:48 +0100

    update schema to capture environment type

--- variables.tf
+++ variables.tf
@@ -37,6 +37,7 @@ variable "install_certs" { default = true }
 variable "create_dns" { default = true }
 variable "custom_worker_img" { default = "" }
 variable "select_cust_worker_img" { default = false }
+variable "environment_type" { default = "" }
 
 locals {
   compute_flexible_shapes   = ["VM.Standard.E3.Flex"]
