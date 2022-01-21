ccb4ea537658eb2ce08953f86637c8e054a000e6
Kyle Francis <kyle.francis@oracle.com>
2022-01-21 13:08:40 -0600

    Fix
    - add resource oci_core_default_route_table
    - no need for ssh key
    - remove default settings like oci_core_subnet/prohibit_public_ip_on_vnic, oke_cluster_ngw/block_traffic and cluster bloat settings
    - Reference stack for default cluster from oci cluster to figure out bloat

--- oke_variables.tf
+++ oke_variables.tf
@@ -6,4 +6,4 @@ variable oke_dp_node_count { default = 3 }
 #file storage
 variable file_storage_work_path { default = "/work" }
 variable file_storage_data_path { default = "/data" }
-variable node_pool_ssh_public_key { default = "" }
+#variable node_pool_ssh_public_key { default = "" }
