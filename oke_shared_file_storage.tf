a3436052dc5dc30edd89fcb88d36cb148cc516d6
Tom Yip <tom.yip@oracle.com>
2022-01-28 01:02:20 -0800

    Re-fixing the compartment ID issue as the old file was removed.

--- oke_shared_file_storage.tf
+++ oke_shared_file_storage.tf
@@ -2,17 +2,15 @@
 # ------ Create File Storage File System
 resource oci_file_storage_file_system work_fss {
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[2].name
-  compartment_id      = var.compartment_ocid
+  compartment_id      = local.Sp3_cid
   display_name = "File System for Work"
 }
 
 resource oci_file_storage_file_system data_fss {
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[2].name
-  compartment_id      = var.compartment_ocid
+  compartment_id      = local.Sp3_cid
   display_name = "File System for Data"
 }
-
-
 
 
 # ------ Create File Storage Export
@@ -66,7 +64,7 @@ resource oci_file_storage_export_set file_storage_export_set_data_sp3 {
 # ------ Create Mount Targets
 resource oci_file_storage_mount_target file_storage_mount_sp3_target {
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[2].name
-  compartment_id      = var.compartment_ocid
+  compartment_id      = local.Sp3_cid
   display_name = "File Storage Mount Target for SP3"
   subnet_id = oci_core_subnet.Privsn001.id
   #ip_address = "10.0.1.86"
@@ -76,7 +74,7 @@ resource oci_file_storage_mount_target file_storage_mount_sp3_target {
 
 resource oci_file_storage_mount_target file_storage_mount_oke_target {
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[2].name
-  compartment_id      = var.compartment_ocid
+  compartment_id      = local.Sp3_cid
   display_name = "File Storage Mount Target for OKE"
   subnet_id = oci_core_subnet.oke_nodes_subset.id
   #ip_address = "10.0.10.86"
