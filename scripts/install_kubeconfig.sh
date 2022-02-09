32c3e5c1bda4932d8587d814014bf55d461d58d5
Tom Yip <tom.yip@oracle.com>
2022-02-08 16:04:43 -0800

    Update the way we install kubectl, and added some checks to make sure head node can use kubectl to access OKE.
    Adding back the original code
    
    Updated comments

--- scripts/install_kubeconfig.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Setup kube config -> /home/ubuntu/.kube/config
-
-export OCI_CLI_AUTH=instance_principal
-oci ce cluster create-kubeconfig --cluster-id ${oke_cluster_id} --auth instance_principal
