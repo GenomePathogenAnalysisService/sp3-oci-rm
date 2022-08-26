954907aeb139e40630611418a0d90697e26b6c83
Jez Swann <jeremy.swann@ndm.ox.ac.uk>
2022-08-26 10:03:56 +0100

    Making cluster dependent on bastion and head node to help deletions.

--- oke_cluster_engine.tf
+++ oke_cluster_engine.tf
@@ -22,6 +22,11 @@ resource oci_containerengine_cluster oke_containerengine_cluster {
     ]
   }
   vcn_id = local.Sp3_vcn_id
+
+  depends_on = [
+    oci_core_instance.Sp3Bastion,
+    oci_core_instance.Sp3Headnode
+  ]
 }
 
 locals {
